8863689cd75cd157e6a7a68ab2994d2e6f5e352c
wsconf <wsconf.org@gmail.com>
2017-06-21 17:41:40 +0900

    older(2017.03) update

---
+++ older/2017_vol1/js/site.js
@@ -0,0 +1,54 @@
+$(function() {
+    var changeColor = (function() {
+        'use strict';
+
+        var changeColor = {
+            init: function() {
+                this.initVar();
+                this.addHandler();
+            },
+            initVar: function() {
+                this.tolerance = -($('body').height() * 0.4);
+                this.$when = $('#when');
+                this.$schedule = $('#schedule');
+                this.$speakers = $('#speakers');
+                this.$sponsors = $('#sponsors');
+                this.$location = $('#location');
+                this.$tickets = $('#tickets');
+                this.$qna = $('#qna');
+            },
+            isViewport: function(element) {
+                return element.is(':in-viewport(' + this.tolerance + ')');
+            },
+            checkViewport: function() {
+
+                var sectionList = ['when', 'schedule', 'speakers', 'sponsors', 'location', 'tickets', 'qna'],
+                    i = 0,
+                    len = sectionList.length;
+
+                for (; i < len; i++) {
+                    if (this.isViewport(this['$' + sectionList[i]])) {
+                        $('body').addClass(sectionList[i]);
+                    } else {
+                        $('body').removeClass(sectionList[i]);
+                    }
+                }
+            },
+            addHandler: function() {
+                var _this = this;
+
+                $(window).on('load resize', function() {
+                    _this.tolerance = -($('body').height() * 0.4);
+                });
+
+                $(window).on('scroll', function() {
+                    _this.checkViewport();
+                });
+            }
+        };
+
+        return changeColor;
+    }());
+
+    changeColor.init();
+});
